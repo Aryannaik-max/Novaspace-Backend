bc4bd0ea3176b764c3e108fa8e780e6ced57ab45
Aryannaik-max <naikaryan949@gmail.com>
2026-01-19 23:41:29 +0530

    imporved cors policy

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ const apiRoutes = require('./routes/index');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://novaspace-a7he.vercel.app/"
-    ],
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://novaspace-seven.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+
+app.options("*", cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
