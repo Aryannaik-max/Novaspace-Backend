f580aa8dc0abc1055d722075c9949a0e93e23a64
Aryannaik-max <naikaryan949@gmail.com>
2026-01-23 19:17:59 +0530

    resovled name issue in config file

--- src/config/config.js
+++ src/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.MYSQL_USERNAME,
+    username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     host: process.env.MYSQL_HOST,
