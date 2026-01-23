0054f45f38235a96851d263eda8a1c83df488aa2
Aryannaik-max <naikaryan949@gmail.com>
2026-01-23 18:43:42 +0530

    replaced config.json with config.js

---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "novaspace_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
